3b6229f29cff628200ebbec3186927de25138deb
Will Wright <willjwright@mac.com>
2012-03-13 18:44:34 -0700

    Updated to 1.2

--- Webz/WebzEntityListener.java
+++ Webz/WebzEntityListener.java
@@ -1,35 +1,30 @@
-/*    */ package Webz;
-/*    */ 
-/*    */ import java.util.List;
-/*    */ import org.bukkit.entity.Spider;
-/*    */ import org.bukkit.event.entity.EntityDeathEvent;
-/*    */ import org.bukkit.event.entity.EntityListener;
-/*    */ import org.bukkit.inventory.ItemStack;
-/*    */ 
-/*    */ public class WebzEntityListener extends EntityListener
-/*    */ {
-/*    */   public static Webz plugin;
-/*    */ 
-/*    */   public WebzEntityListener(Webz instance)
-/*    */   {
-/* 21 */     plugin = instance;
-/*    */   }
-/*    */ 
-/*    */   public void onEntityDeath(EntityDeathEvent event) {
-/* 25 */     if ((event.getEntity() instanceof Spider))
-/*    */     {
-/* 27 */       List drops = event.getDrops();
-/*    */ 
-/* 29 */       for (int i = 0; i < drops.size(); i++) {
-/* 30 */         ItemStack drop = (ItemStack)drops.get(i);
-/* 31 */         if (drop.getTypeId() == 287)
-/* 32 */           drop.setTypeId(30);
-/*    */       }
-/*    */     }
-/*    */   }
-/*    */ }
-
-/* Location:           /Users/Will/Downloads/Webz.jar
- * Qualified Name:     Webz.WebzEntityListener
- * JD-Core Version:    0.6.0
- */
+ package Webz;
+ 
+ import java.util.List;
+ import org.bukkit.entity.Spider;
+ import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+ import org.bukkit.inventory.ItemStack;
+ 
+ public class WebzEntityListener implements Listener
+ {
+   public static Webz plugin;
+ 
+   public WebzEntityListener(Webz instance)
+   {
+     plugin = instance;
+   }
+ 
+   public void onEntityDeath(EntityDeathEvent event) {
+     if ((event.getEntity() instanceof Spider))
+     {
+       List drops = event.getDrops();
+ 
+       for (int i = 0; i < drops.size(); i++) {
+         ItemStack drop = (ItemStack)drops.get(i);
+         if (drop.getTypeId() == 287)
+           drop.setTypeId(30);
+       }
+     }
+   }
+ }
